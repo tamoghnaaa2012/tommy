1dd83baad2963ae253684c98185139ad1a6d4bb0
Tamoghna Pattanayak <42137018+tamoghnaaa2012@users.noreply.github.com>
2018-09-06 12:13:08 -0400

    Add files via upload

---
+++ sample1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n=5;
+	for(int i = 0; i < 5 ; i++){
+	
+      //print stars in square 
+        int j = 0;
+        while( j < (5)){
+            cout << "*";
+        	j++;
+       
+        }  
+        cout << endl;
+	}
+return 0;
+}
